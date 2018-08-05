b75b491c124f6ce72624f538ea7d67f5a05993dc
Navid <navidshad72@gmail.com>
2018-08-05 13:33:16 +0430

    add user to command event

--- bot/routting/messageRouting.js
+++ bot/routting/messageRouting.js
@@ -14,64 +14,68 @@ var routting = async function(message){
     {
         if(message.text.startsWith('/start')) 
             await fn.commands.start(message);
-        fn.eventEmitter.emit('commands', message);
+
+        //validating user
+        var user = await fn.userOper.checkProfile(message.from.id).then();
+        if(!user) return;
+
+        // emit command
+        fn.eventEmitter.emit('commands', message, user);
     }
     
     //routting
     else
     {
         //validating user
-        fn.userOper.checkProfile(message.from.id, (user) => {
-            
-            if(!user) return;
+        var user = await fn.userOper.checkProfile(message.from.id).then();
+        if(!user) return;
 
-            //sperate section 
-            var speratedSection = user.section.split('/');
-            
-            //go to meain menu
-            if(message.text && message.text === fn.str['backToMenu']) fn.commands.backToMainMenu(message.from.id, user);
-            
-            //when profile is compelet
-            else if(user.isCompelet){
-                //console.log('user profile is compelet');
-                //text message
-                if(message.text)
-                {
-                    var text = message.text;
-                    
-                    //go to admin
-                    if(text === fn.str.goToAdmin['name'] || text === fn.str.goToAdmin['back'] || speratedSection[1] === fn.str.goToAdmin['name'] && user.isAdmin){
-                        fn.adminPanel.routting(message, speratedSection, user);
-                    }
-                    
-                    //chanelChecker
-                    else if(!user.isAdmin && !user.isMemberOfChannel) {
-                        fn.m.chanelChecker.InviteUser(message.from.id); 
-                        return;
-                    }
-
-                    //menu items
-                    else if(text === fn.mstr.category['backtoParent'] || fn.checkValidMessage(text, global.robot.menuItems) || fn.checkValidMessage(speratedSection[1], global.robot.menuItems))
-                        fn.menu.routting(message, speratedSection, user);
-
-                    //free message
-                    else fn.freeStrings.routting(message, speratedSection, user);
+        //sperate section 
+        var speratedSection = user.section.split('/');
+        
+        //go to meain menu
+        if(message.text && message.text === fn.str['backToMenu']) fn.commands.backToMainMenu(message.from.id, user);
+        
+        //when profile is compelet
+        else if(user.isCompelet){
+            //console.log('user profile is compelet');
+            //text message
+            if(message.text)
+            {
+                var text = message.text;
+                
+                //go to admin
+                if(text === fn.str.goToAdmin['name'] || text === fn.str.goToAdmin['back'] || speratedSection[1] === fn.str.goToAdmin['name'] && user.isAdmin){
+                    fn.adminPanel.routting(message, speratedSection, user);
+                }
+                
+                //chanelChecker
+                else if(!user.isAdmin && !user.isMemberOfChannel) {
+                    fn.m.chanelChecker.InviteUser(message.from.id); 
+                    return;
                 }
 
-                else if(message.contact)
-                    fn.freeStrings.routting(message, speratedSection, user);
+                //menu items
+                else if(text === fn.mstr.category['backtoParent'] || fn.checkValidMessage(text, global.robot.menuItems) || fn.checkValidMessage(speratedSection[1], global.robot.menuItems))
+                    fn.menu.routting(message, speratedSection, user);
 
-                //non text message
-                else fn.upload(message, speratedSection, user);
+                //free message
+                else fn.freeStrings.routting(message, speratedSection, user);
             }
-            //user profile is not compelet
-            else if (!isCompelet && message.text){
-                //console.log('user profile is not compelet', 'you should register first');
-                var name = 'کاربر گرامی ' + message.from.first_name + ' عزیر،';
-                var pm = name + '\n' + 'شما ابتدا باید پروفایل خود را تکمیل کنید تا بتوانید از امکانات ربات استفاده کنید.';
-                global.fn.sendMessage(message.from.id, pm, fn.generateKeyboard({section:fn.str['mainMenu'], "isCompelet": isCompelet}, false));
-            }
-        });
+
+            else if(message.contact)
+                fn.freeStrings.routting(message, speratedSection, user);
+
+            //non text message
+            else fn.upload(message, speratedSection, user);
+        }
+        //user profile is not compelet
+        else if (!isCompelet && message.text){
+            //console.log('user profile is not compelet', 'you should register first');
+            var name = 'کاربر گرامی ' + message.from.first_name + ' عزیر،';
+            var pm = name + '\n' + 'شما ابتدا باید پروفایل خود را تکمیل کنید تا بتوانید از امکانات ربات استفاده کنید.';
+            global.fn.sendMessage(message.from.id, pm, fn.generateKeyboard({section:fn.str['mainMenu'], "isCompelet": isCompelet}, false));
+        }
     }
 }
 
